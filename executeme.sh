a4ac3ae93f6dd03554712b6644d32e25930cbf79
Cesar-Morales <moraleescesar@gmail.com>
2021-10-24 23:10:16 -0300

    creando simplificador

---
+++ executeme.sh
@@ -0,0 +1,25 @@
+cat <<EOT >> index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <script src="script.js"></script>
+</body>
+</html>
+EOT
+
+cat <<EOT >> script.js
+console.log("here")
+EOT
+
+cat <<EOT >> style.css
+body{
+    background-color: black;
+}
+EOT
